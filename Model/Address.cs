2b2de8e0a524811d5943941df86a0f67bdce33db
Rahul Bhatia <Rahul_Bhatia@mckinsey.com>
2018-07-24 18:37:16 +0530

    AMS base commit

---
+++ Model/Address.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AMS.Model
+{
+    public class Address
+    {
+        public string FlatNumber
+        {
+            get;
+            set;
+        }
+
+        public string Town
+        {
+            get;
+            set;
+        }
+
+        public string Society
+        {
+            get;
+            set;
+        }
+
+        public string State
+        {
+            get;
+            set;
+        }
+
+        public string PinCode
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Model/Devotee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+
+namespace AMS.Model
+{
+    public class Devotee
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public Address Address
+        {
+            get;set;
+        }
+
+        public string MobileNumber
+        {
+            get;
+            set;
+        }
+
+        public MemoryStream Picture
+        {
+            get;
+            set;
+        }
+
+        public int AshrayGroup
+        {
+            get;
+            set;
+        }
+
+    }
+}
